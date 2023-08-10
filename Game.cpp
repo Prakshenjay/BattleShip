2302cb9b5016d176604d3ba42227fe7c1f9a052f
Prakshenjay Bhati <prakshenjay@gmail.com>
2023-08-11 03:15:54 +0530

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,14 +1,22 @@
-
-#include "Game.h"
 #include <iostream>
-
-Game::Game()
-{
-    //
-}
+#include <string>
+#include <iomanip>
+#include "Game.hpp"
 
 void Game::setup()
 {
+    system("clear");
+    
+    std::string name;
+    std::cout << "Enter Player's Name : ";
+    std::cin >> name;
+    player.giveName(name);
+    
+    std::cout << "Place your ships" << std::endl;
+    player.placeShips();
+
+    computer.giveName("Computer");
+    
     std::cout << "Computer is placing its ships.\n";
     computer.placeShips();
 
@@ -22,12 +30,60 @@ void Game::play()
 
     while (true)
     {
-        system("cls");
+        system("clear");
 
-        for()
+        std::cout << "\n";
+        for(int k = 0 ; k < 2 ; k++)
+            {
+                std::cout << "\t\t  ";
+                for(int j = 0 ; j < 10 ; j++)
+                {
+                    std::cout << " " << j << " ";
+                }
+                std::cout << "\t\t";
+            }
+        std::cout << "\n";
+        
+        for(int k = 0 ; k < 2 ; k++)
+        {
+            std::cout << "\t\t  ";
+            for(int j = 0 ; j < 10 ; j++)
+            {
+                std::cout << "---";
+            }
+            std::cout << "\t\t";
+        }
+        std::cout << "\n";
+
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            std::cout << "\t\t" << static_cast<char>('A' + i) << "|" ;
+            for(int j = 0 ; j < 10 ; j++)
+            {
+                std::cout << " " << playerBoard.Grid[i][j] << " " ;
+            }
+            std::cout << "\t\t";
+            
+            std::cout << "\t\t" << static_cast<char>('A' + i) << "|" ;
+            for(int j = 0 ; j < 10 ; j++)
+            {
+                std::cout << " " << computerBoard.Grid[i][j] << " " ;
+            }
+            std::cout << "\t\t";
+            
+            std::cout << "\n";
+        }
+
+        std::cout << "\n";
+        std::cout << "\t\t" << std::setw(25) << std::left << player.getName() + "'s Board";
+        std::cout << "\t\t\t\t\t" << "Computer's Board";
+        
+        //--------------------------------------------------------------------------------//
+        //--------------------------------------------------------------------------------//
+        
         std::cout << "\n" << currentPlayer->getName() << "'s Turn:\n";
 
-        bool turnOver = currentPlayer->takeTurn(*opponent);
+        bool turnOver = currentPlayer->takeTurn();
 
         if (opponent->hasLost())
         {
