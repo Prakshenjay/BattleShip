7b2db7d1b080def5f4a111f6ee2b123daa63fe27
Prakshenjay Bhati <prakshenjay@gmail.com>
2023-08-11 16:44:12 +0530

    Update Computer.hpp

--- Computer.hpp
+++ Computer.hpp
@@ -2,14 +2,17 @@
 #define COMPUTER_HPP
 
 #include "Player.hpp"
+#include <random>
 
 class Computer : public Player
 {
 private :
+        std::mt19937 rng;
 
 public :
-        Board computerBoard;
+        Computer();
         void placeShips();
+        void takeTurn(Player& opponent);
 };
 
 #endif
