a6f43f8710483d10cc3a94cc21eba753b7d0f2bf
Prakshenjay Bhati <prakshenjay@gmail.com>
2023-08-11 16:44:20 +0530

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -3,19 +3,26 @@
 
 #include <string>
 
+#define BOARD_SIZE 10
+
 class Player
 {
 private :
         std::string playerName;
-        int shipLength[5] = {3,3,4,4,5};
         
 public :
-        Board playerBoard;
+        char Grid[BOARD_SIZE][BOARD_SIZE];
+        int shipLength[5] = {3,3,4,4,5};
+        
+        Player();
+        
         void giveName(std::string name);
-        void placeShips();
-        bool takeTurn();
-        bool hasLost();
         std::string getName();
+        
+        void display();
+        void placeShips();
+        virtual void takeTurn(Player& opponent);
+        bool hasLost();
 };
 
 #endif
